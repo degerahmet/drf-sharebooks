b13b483a9938296cd9decf2cba889569cdb4d9d2
Ahmet <adeger34@gmail.com>
2021-10-13 17:03:21 +0300

    root static added

--- sharebooks/settings.py
+++ sharebooks/settings.py
@@ -138,12 +138,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/home/degerahmet/drf-sharebooks/static'
 
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = BASE_DIR / 'media/'
 
-SITE_URL = "http://localhost:8000"
+SITE_URL = "https://degerahmet.pythonanywhere.com"
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
